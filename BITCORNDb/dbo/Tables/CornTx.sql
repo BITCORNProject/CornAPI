446c9698bf16648c25b729ada158e8ad39e70f3c
Jason Sowers <jason.w.sowers@gmail.com>
2019-11-23 22:28:37 -0800

    added corntx table

---
+++ BITCORNDb/dbo/Tables/CornTx.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CornTransaction]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Platform] VARCHAR(50) NULL, 
+    [TxType] NVARCHAR(50) NULL, 
+    [Amount] NUMERIC(19, 8) NULL, 
+    [SenderId] VARCHAR(100) NULL, 
+    [ReceiverId] VARCHAR(100) NULL, 
+    [Timestamp] DATETIME NULL
+)
