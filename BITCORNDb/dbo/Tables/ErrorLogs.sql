c588468f173324fa5ed1ed0fc58db924d10ac52a
Jason Sowers <jason.w.sowers@gmail.com>
2019-11-23 20:47:27 -0800

    added errorlogs table and other updates to tables

---
+++ BITCORNDb/dbo/Tables/ErrorLogs.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ErrorLogs]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [Application] VARCHAR(100) NULL, 
+    [Message] VARCHAR(1000) NULL, 
+    [StackTrace] VARCHAR(5000) NULL, 
+    [Code] VARCHAR(100) NULL, 
+    [TImestamp] DATETIME NULL
+)
--- BITCORNDb/dbo/Tables/User.sql
+++ BITCORNDb/dbo/Tables/User.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[User] (
     [UserId]   INT            IDENTITY (420000000, 1) NOT NULL,
     [Level]    VARCHAR (50)   NULL,
-    [Username] NVARCHAR (50)  NULL,
+    [Username] VARCHAR(50)  NULL,
     [Avatar]   VARCHAR (2048) NULL,
     PRIMARY KEY CLUSTERED ([UserId] ASC)
 );
