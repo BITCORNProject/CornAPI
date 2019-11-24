8d86ee105d967a7ba46e77368db71898eb435763
Jason Sowers <jason.w.sowers@gmail.com>
2019-11-23 16:37:35 -0800

    initial commit

---
+++ CornAPI/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CornAPI.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Auth0Id { get; set; }
+        public string Auth0Nickname { get; set; }
+        public string Twitchid { get; set; }
+        public string Discordid { get; set; }
+        public string Twitterid { get; set; }
+        public string Redditid { get; set; }
+        public string TwitchUsername { get; set; }
+        public string Cornaddy { get; set; }
+        public int Walletserver { get; set; }
+        public decimal Balance { get; set; }
+        public string Level { get; set; }
+        public string Avatar { get; set; }
+        public string Subtier { get; set; }
+       
+    }
+}
---
+++ CornAPI/Models/UserStat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CornAPI.Models
+{
+    public class UserStat
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public int UserId { get; set; }
+        public int? Rains { get; set; }
+        public int? Rainedon { get; set; }
+        public int? Tips { get; set; }
+        public decimal? Tiptotal { get; set; }
+        public decimal? Raintotal { get; set; }
+        public decimal? Rainontotal { get; set; }
+    }
+}
